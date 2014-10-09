cb4c00010db42ca637394923171f5703c8820966
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-10-09 21:19:56 +0700

    fix table

--- db/migrate/20141009070636_create_exercises_trainers.rb
+++ db/migrate/20141009070636_create_exercises_trainers.rb
@@ -1,9 +1,8 @@
 class CreateExercisesTrainers < ActiveRecord::Migration
   def change
     create_table :exercises_trainers, id: false do |t|
-      t.integer :trainer_id
-      t.integer :exercise_id
-      t.timestamps
+      t.belongs_to :exercise
+      t.belongs_to :trainer
     end
   end
 end
