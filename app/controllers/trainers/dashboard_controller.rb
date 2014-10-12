fc789dbaf865b0596d2bb1c89e59363fde57c8f3
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-10-12 16:37:26 +0700

    refactor dashboard

--- app/controllers/trainers/dashboard_controller.rb
+++ app/controllers/trainers/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class Trainers::DashboardController < TrainersController
 	before_filter :authenticate_trainer!
+	before_filter :load_trainer
 	def index
-		
+	end
+
+	private
+
+	def load_trainer
+		@trainer ||= current_trainer.decorate
 	end
 end
