06204096198c3dd8b62b138f18bd681967eee30a
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-10-12 17:31:54 +0700

    add trainer/assistant

--- app/decorators/trainer_decorator.rb
+++ app/decorators/trainer_decorator.rb
@@ -2,11 +2,10 @@ class TrainerDecorator < Draper::Decorator
   delegate_all
   decorates TrainersController
   def is_trainer
-    if is_trainer?
-      "Trainer"
-    else
-      "Assistant"
-    end
+    is_trainer? ? "Trainer" : "Assistant"
   end
-
+  
+  def logout_link
+    # link_to "Logout", destroy_trainer_session_path(self), method: :destroy if trainer_signed_in?
+  end
 end
