7244f5ad77a17e78df8ee0b4e788bb8c1f50bb59
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-10-13 21:42:34 +0700

    add init_trainer

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,3 +1,10 @@
 class TrainersController < ApplicationController 
   layout 'dashboard'
+  before_filter :init_trainer
+  
+  private
+  
+  def init_trainer
+  	@trainer ||= current_trainer.decorate
+  end
 end
