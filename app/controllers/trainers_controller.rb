80a4c6b40b51650b5415c78075bfc4bdfbe650ce
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-10-14 20:22:33 +0700

    fix spaces

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,11 +1,11 @@
 class TrainersController < ApplicationController 
-	before_filter :authenticate_trainer!
+  before_filter :authenticate_trainer!
   before_filter :init_trainer
   layout 'dashboard'
   
   private
 
   def init_trainer
-  	@trainer ||= current_trainer.decorate
+    @trainer ||= current_trainer.decorate
   end
 end
