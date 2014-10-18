5b670e18d60e9b0122c098fcb9c37e51f780bb0f
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-10-18 15:14:59 +0700

    add search trainers params

--- app/controllers/users/books_controller.rb
+++ app/controllers/users/books_controller.rb
@@ -8,6 +8,13 @@ class Users::BooksController < ApplicationController
   end
 
   def find_trainer
-    
+    @trainers = Trainer.available
   end
+
+  private
+
+  def search_trainer_params
+    params.permit(:book_date, :book_time, :book_range, :book_location, :book_exercise, :book_is_trainer)
+  end
+
 end
