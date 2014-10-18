43be3b13faa15ee1f8b821557a85c7779cee95e1
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-10-18 15:25:35 +0700

    fix seeds

--- db/seeds/development/04_sessions.rb
+++ db/seeds/development/04_sessions.rb
@@ -1,8 +1,8 @@
 if Session.count.zero?
   trainers = Trainer.joins(:exercises)
   users = User.all.pluck(:id)
-  start_session = Time.now + (1..7).to_a.sample.days + 3.hours
   15.times do
+    start_session = Time.now + (1..7).to_a.sample.days + 3.hours
     Session.create(trainer_id: trainers.pluck(:id).sample, user_id: users.sample, from_when: start_session, to_when: start_session + 45.minutes, is_private: [1, 0].sample, status: true)
   end 
 end
