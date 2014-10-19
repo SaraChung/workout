265475edfa4ff6d02233c5f6afb5d9754eaedf8f
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-10-19 11:03:05 +0700

    refactoring trainers finder

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -17,10 +17,10 @@ class Trainer < ActiveRecord::Base
   validates :phone_number, presence: true
   
   scope :relations, -> { eager_load(:exercises, :sessions) }
-  scope :skill, -> (id) { relations.where("exercises.id = ?", id).pluck(:id).uniq }
-  scope :has_session, -> (start_date, end_date) { relations.where("sessions.from_when >= ? AND sessions.to_when <= ?",start_date, end_date).pluck(:id).uniq }
+  scope :skill, -> (id) { relations.where("exercises.id = ?", id) }
+  scope :has_session, -> (start_date, end_date) { relations.where("sessions.from_when >= ? AND sessions.to_when <= ?",start_date, end_date) }
   scope :closest_trainer, -> (user_address) { near(user_address, 10, units: :km) }
-  scope :available, -> (id, start_date, end_date) { where("id IN (#{(skill(id) - has_session(start_date, end_date)).join(',')})") }
+  scope :available, -> (id, start_date, end_date, trainer_assistant, address) { where("id IN ((SELECT trainers.id FROM trainers LEFT OUTER JOIN exercises_trainers ON exercises_trainers.trainer_id = trainers.id LEFT OUTER JOIN exercises ON exercises.id = exercises_trainers.exercise_id LEFT OUTER JOIN sessions ON sessions.trainer_id = trainers.id WHERE (exercises.id = ?) AND trainers.id NOT IN (SELECT trainers.id FROM trainers LEFT OUTER JOIN exercises_trainers ON exercises_trainers.trainer_id = trainers.id LEFT OUTER JOIN exercises ON exercises.id = exercises_trainers.exercise_id LEFT OUTER JOIN sessions ON sessions.trainer_id = trainers.id WHERE (sessions.from_when >= ? AND sessions.to_when <= ? )))) AND trainers.is_trainer = ?", id, start_date, end_date, trainer_assistant).near(address) }
 
   def load_trainer_skills
     self.exercises.pluck(:id)
