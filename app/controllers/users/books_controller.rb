c5653416b746c24963b631d61c8aa7e360c242fb
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-10-28 21:06:43 +0700

    add create sessions and user on booking

--- app/controllers/users/books_controller.rb
+++ app/controllers/users/books_controller.rb
@@ -1,12 +1,10 @@
 class Users::BooksController < ApplicationController
   respond_to :html, :js, only: [:find_trainer]
 
-  def new
-    
-  end
-
   def create
-    
+    set_user
+    set_session
+    if @user.persisted? && @session.persisted?
   end
 
   def find_trainer
@@ -18,4 +16,17 @@ class Users::BooksController < ApplicationController
   def to_date(date)
     DateTime.new(date[:year].to_i, date[:month].to_i, date[:day].to_i, date[:hour].to_i, date[:minute].to_i)
   end
+
+  def set_params
+    
+  end
+
+  def set_user
+    @user = User.create(first_name: params[:booker_first_name], last_name: params[:booker_last_name], email: params[:booker_email], phone_number: params[:booker_phone_number])
+  end
+
+  def set_session
+    @session = Session.create(trainer_id: params[:booker_which_trainer], user_id: @user.id, exercise_id: params[:booker_exercise], from_when: params[:booker_from], to_when: params[:booker_to], status: 0, address: params[:])
+  end
+
 end
