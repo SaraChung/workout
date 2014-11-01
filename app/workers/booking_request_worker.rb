14a0d60fc82abb188f9ad76798e91d0cbc998c3a
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-01 16:49:25 +0700

    rename class

---
+++ app/workers/booking_request_worker.rb
@@ -0,0 +1,15 @@
+class BookingRequestWorker
+  include Sidekiq::Worker
+  include Sidekiq::Benchmark::Worker
+  
+  def perform(user_id, trainer_id)
+    benchmark.sample do
+      1.times do
+        user = User.find(user_id)
+        trainer = Trainer.find(trainer_id)
+        push_notification = NotificationsService.new
+        push_notification.push_booking_request(user, trainer)
+      end
+    end
+  end
+end
--- app/workers/booking_worker.rb
+++
@@ -1,9 +0,0 @@
-class BookingWorker
-  include Sidekiq::Worker
-  def perform(user_id, trainer_id)
-    user = User.find(user_id)
-    trainer = Trainer.find(trainer_id)
-    push_notification = NotificationsService.new
-    push_notification.push_booking_notification(user, trainer)
-  end
-end
