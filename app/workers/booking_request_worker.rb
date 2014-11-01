bb0b8ca9c96a991995b14dd4d6e39464e0919860
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-02 05:27:02 +0700

    fix worker

--- app/workers/booking_request_worker.rb
+++ app/workers/booking_request_worker.rb
@@ -1,15 +1,14 @@
 class BookingRequestWorker
   include Sidekiq::Worker
-  include Sidekiq::Benchmark::Worker
+  # include Sidekiq::Benchmark::Worker
   
   def perform(user_id, trainer_id)
-    benchmark.sample do
-      1.times do
+    # benchmark.sample do
+      # 1.times do
         user = User.find(user_id)
-        trainer = Trainer.find(trainer_id)
+        trainer = Trainer.find(trainer_id.to_i)
         push_notification = NotificationsService.new
         push_notification.push_booking_request(user, trainer)
-      end
-    end
+      # end
   end
 end
