26657d3cea186d57f18dd501f1cdb763ddc3d539
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-02 05:26:43 +0700

    add more assets on load

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,5 +5,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( dashboard.css )
-Rails.application.config.assets.precompile += %w( dashboard.js home/index.js )
+Rails.application.config.assets.precompile += %w( dashboard.css user_dashboard.css)
+Rails.application.config.assets.precompile += %w( dashboard.js user_dashboard.js home/index.js )
