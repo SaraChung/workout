7fb10a81b3bd2e71e7ea9edcb40156d71c29e839
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-02 05:28:41 +0700

    modified notification service

--- app/services/notifications_service.rb
+++ app/services/notifications_service.rb
@@ -1,5 +1,10 @@
 class NotificationsService
   def push_booking_request(user, trainer)
-    
+    type = NotificationsType.what_id("Booking Request")
+    Notification.create(notifications_type_id: type, subject_id: user.id, object_id: trainer.id, title: "You have new booking request!", short_message: "[#{user.email}] send a booking request to you.", long_message: "[#{user.email}] send a booking request to you. #{its(user.first_name)} phone number is #{user.phone_number}.")
+  end
+
+  def its(name)
+    name.last == "s" ? name + "'" : name + "'s"
   end
 end
