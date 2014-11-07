c40cf4d983918646e4f11bc6f6b45ae4177d30f5
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-07 20:46:39 +0700

    add scope and geocode

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  geocoded_by :address
+  after_validation :geocode
 
   has_many :sessions
   has_many :trainers, through: :sessions
   
+  scope :where_is, -> (email) { where("email = ?", email).pluck(:address)[0] }
+
   before_validation do
     self.password = self.phone_number
   end
