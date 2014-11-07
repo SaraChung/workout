b8154213e2e91936e8ecbb501c6ce2bb5a88eb52
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-07 20:54:06 +0700

    modify public functions

--- app/assets/javascripts/public/functions.js
+++ app/assets/javascripts/public/functions.js
@@ -8,10 +8,27 @@ var get_book = function(){
     $("#booker_location").val($("#book_location").val());
 }
 
+var get_registered_book = function(trainer_id){
+    dateFrom = moment({ y: $("#rbook_time_year").val(), M: $("#rbook_time_month").val(), d: $("#rbook_time_day").val(), h: $("#rbook_time_hour").val(), m: $("#rbook_time_minute").val() }).format("YYYY-M-DD HH:mm");
+    dateTo = moment(dateFrom).add($("#rbook_range").val(), 'm').format("YYYY-M-DD HH:mm");
+    $("#rbook_from").val(dateFrom);
+    $("#rbook_to").val(dateTo);
+    $("#rbook_trainer").val(trainer_id);
+}
+
 var clear_book_form = function(){
-  $("#first_time_use_form").change(function(){
-    $(".trainer-list").html("");
-    $("#booking_form").find('input[type="hidden"]').val("");
-    $(".booking-fieldset").hide();
-  });
+    $("#first_time_use_form").change(function(){
+        $(".trainer-list").html("");
+        $("#booking_form").find('input[type="hidden"]').val("");
+        $(".booking-fieldset").hide();
+    });
+}
+
+var registered_user_booking = function(){
+    dateFrom = moment({ y: $("#rbook_time_year").val(), M: $("#rbook_time_month").val(), d: $("#rbook_time_day").val(), h: $("#rbook_time_hour").val(), m: $("#rbook_time_minute").val() }).format("YYYY-M-DD HH:mm");
+    dateTo = moment(dateFrom).add($("#rbook_range").val(), 'm').format("YYYY-M-DD HH:mm");
+    $("#find_trainer").click(function(){
+        alert(dateFrom);
+        $.ajax({ url: "/users/registered_books/find_trainer?exercise="+$("#rbook_exercise").val()+"&from="+dateFrom+"&to="+dateTo+"&trainer="+$("#rtrainer_is_trainer").val()+"&user_email="+$("#rbook_email").val() });
+    });
 }
