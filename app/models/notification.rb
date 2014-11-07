36abf3a4e41d785ced7f1dbd266f87a3a7a38b52
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-07 20:50:59 +0700

    add scope

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,6 @@
 class Notification < ActiveRecord::Base
   belongs_to :notifications_type
+  
+  default_scope { eager_load("notifications_type") }
+  scope :trainer_feeds, -> (id) { where("notifications_types.object = 'Trainer' AND object_id = ?", id) }
 end
