362e2d381844166d454537efa739dfa3444556f2
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-09 03:40:43 +0700

    add scope

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,6 @@ class Notification < ActiveRecord::Base
   belongs_to :notifications_type
   
   default_scope { eager_load("notifications_type") }
-  scope :trainer_feeds, -> (id) { where("notifications_types.object = 'Trainer' AND object_id = ?", id).order("notifications.created_at DESC").limit(10) }
+  scope :trainer_feeds, -> (id) { where("notifications_types.object = 'Trainer' AND object_id = ?", id).order("notifications.created_at DESC") }
+  scope :count_unread, -> (id) { where("notifications_types.object = 'Trainer' AND object_id = ? AND is_read = FALSE", id).count }
 end
