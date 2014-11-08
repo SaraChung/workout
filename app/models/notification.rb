7b0908801ec734071ab4b8b4727be758931c6f5b
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-08 17:28:23 +0700

    modify feeds

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,5 @@ class Notification < ActiveRecord::Base
   belongs_to :notifications_type
   
   default_scope { eager_load("notifications_type") }
-  scope :trainer_feeds, -> (id) { where("notifications_types.object = 'Trainer' AND object_id = ?", id) }
+  scope :trainer_feeds, -> (id) { where("notifications_types.object = 'Trainer' AND object_id = ?", id).order("notifications.created_at DESC").limit(10) }
 end
