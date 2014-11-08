910b6a647eb13d6847b03753f6cfb32462a0b009
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-09 03:40:08 +0700

    add gone watch

--- app/controllers/trainers/dashboard_controller.rb
+++ app/controllers/trainers/dashboard_controller.rb
@@ -2,12 +2,14 @@ class Trainers::DashboardController < TrainersController
   before_filter :load_feeds, only: :index
 
   def index
-    
+    gon.watch.notif = @notifications
+    gon.watch.count_unread = @count_unread
   end
 
   private
 
   def load_feeds
-    @notifications ||= Notification.trainer_feeds(current_trainer.id)
+    @notifications ||= Notification.trainer_feeds(current_trainer.id).limit(10)
+    @count_unread ||= Notification.count_unread(current_trainer.id)
   end
 end
