8250c053cca076be262664821ffc933dadade5d6
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-10 05:46:36 +0700

    add more js

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,8 @@
 //= require jquery
 //= require jquery_ujs
+//= require app/functions
 //= require app/live_reload
+//= require dashboard/ready
 //= require public/navigation
 //= require public/vertical-tabs
 //= require public/accordion-tabs-minimal
