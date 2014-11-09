10916436814e212c65a866e21962a3ba16747f2c
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-10 05:47:35 +0700

    add hr_request for remote request

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,9 +1,13 @@
 class TrainersController < ApplicationController 
   before_filter :authenticate_trainer!
   before_filter :init_trainer
-  layout 'dashboard'
+  layout :xhr_request?
   
   private
+
+  def xhr_request?
+    request.xhr? ? false : 'dashboard'
+  end
 
   def init_trainer
     @trainer ||= current_trainer.decorate
