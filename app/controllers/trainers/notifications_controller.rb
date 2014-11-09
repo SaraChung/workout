3c43b05dbe95977d7ce89d33ceeba2ea8f3abcd1
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-10 05:47:51 +0700

    add notifications controller

---
+++ app/controllers/trainers/notifications_controller.rb
@@ -0,0 +1,24 @@
+class Trainers::NotificationsController < TrainersController
+  respond_to :html, :js, only: [:show, :latest_feeds]
+  before_filter :load_feeds, only: [:index, :latest_feeds]
+  
+  def index
+    
+  end
+
+  def show
+    @notification = Notification.find(params[:notif_id])
+    @notification.update_attributes(is_read: "true")
+  end
+
+  def latest_feeds
+    
+  end
+
+  private
+
+  def load_feeds
+    @notifications ||= Notification.trainer_feeds(current_trainer.id)
+  end
+
+end
