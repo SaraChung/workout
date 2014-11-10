01cf4d1faf307a05e074ec777db285f9143ecf13
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-10 21:04:43 +0700

    refactoring js

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,8 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require app/functions
-//= require app/live_reload
-//= require dashboard/ready
 //= require public/navigation
 //= require public/vertical-tabs
 //= require public/accordion-tabs-minimal
--- app/assets/javascripts/trainers/dashboard.js.coffee
+++ app/assets/javascripts/trainers/dashboard.js.coffee
@@ -3,3 +3,4 @@ renewNotifCount = (count) ->
 
 gon.watch('count_unread', interval: 3000, renewNotifCount)
 gon.watch('notif', { interval: 3000, url: '/trainers/dashboard/latest_feeds'})
+view_notification()
