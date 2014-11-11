d7aa9ccbd447828d3ea6275d23f423cc4c7e68bc
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-11 11:44:21 +0700

    fix trainers search with ajax

--- app/assets/javascripts/app/functions.js
+++ app/assets/javascripts/app/functions.js
@@ -1,6 +1,6 @@
 var get_book = function(){
-    dateFrom = moment({y: $("#book_time_year").val(), M: $("#book_time_month").val(), d: $("#book_time_day").val(), h: $("#book_time_hour").val(), m: $("#book_time_minute").val()});
-    dateTo = moment(dateFrom).add($("#book_range").val(), 'm');
+    dateFrom = moment($("#book_time_year").val()+"-"+$("#book_time_month").val()+"-"+$("#book_time_day").val()+"T"+$("#book_time_hour").val()+":"+$("#book_time_minute").val()).format("YYYY-M-DD h:mm");
+    dateTo = moment(dateFrom).add($("#book_range").val(), 'm').format("YYYY-M-DD h:mm");
     $("#booker_from").val(dateFrom);
     $("#booker_to").val(dateTo);
     $("#booker_trainer").val($("#trainer_is_trainer").val());
@@ -9,8 +9,8 @@ var get_book = function(){
 }
 
 var get_registered_book = function(trainer_id){
-    dateFrom = moment({ y: $("#rbook_time_year").val(), M: $("#rbook_time_month").val(), d: $("#rbook_time_day").val(), h: $("#rbook_time_hour").val(), m: $("#rbook_time_minute").val() }).format("YYYY-M-DD HH:mm");
-    dateTo = moment(dateFrom).add($("#rbook_range").val(), 'm').format("YYYY-M-DD HH:mm");
+    dateFrom = moment($("#rbook_time_year").val()+"-"+$("#rbook_time_month").val()+"-"+$("#rbook_time_day").val()+"T"+$("#rbook_time_hour").val()+":"+$("#rbook_time_minute").val()).format("YYYY-M-DD h:mm");
+    dateTo = moment(dateFrom).add($("#rbook_range").val(), 'm').format("YYYY-M-DD h:mm");
     $("#rbook_from").val(dateFrom);
     $("#rbook_to").val(dateTo);
     $("#rbook_trainer").val(trainer_id);
@@ -22,12 +22,26 @@ var clear_book_form = function(){
         $("#booking_form").find('input[type="hidden"]').val("");
         $(".booking-fieldset").hide();
     });
+
+    $("#registered_user_form").change(function(){
+        $(".trainer-list").html("");
+        $(".booking-fieldset").hide();
+        $("#booking_form").find('input[type="hidden"]').val("");
+    });
+
+    $("#registered_link, #first_time_link").click(function(){
+        $(".trainer-list").html("");
+        $(".booking-fieldset").hide();
+        $("#booking_form").find('input[type="hidden"]').val("");
+        $(this).find('input[type="text"]').val(""); 
+    });
+
 }
 
 var registered_user_booking = function(){
-    dateFrom = moment({ y: $("#rbook_time_year").val(), M: $("#rbook_time_month").val(), d: $("#rbook_time_day").val(), h: $("#rbook_time_hour").val(), m: $("#rbook_time_minute").val() }).format("YYYY-M-DD HH:mm");
-    dateTo = moment(dateFrom).add($("#rbook_range").val(), 'm').format("YYYY-M-DD HH:mm");
     $("#find_trainer").click(function(){
+        dateFrom = moment($("#rbook_time_year").val()+"-"+$("#rbook_time_month").val()+"-"+$("#rbook_time_day").val()+"T"+$("#rbook_time_hour").val()+":"+$("#rbook_time_minute").val()).format("YYYY-M-DD h:mm");
+        dateTo = moment(dateFrom).add($("#rbook_range").val(), 'm').format("YYYY-M-DD h:mm");
         $.ajax({ url: "/users/registered_books/find_trainer?exercise="+$("#rbook_exercise").val()+"&from="+dateFrom+"&to="+dateTo+"&trainer="+$("#rtrainer_is_trainer").val()+"&user_email="+$("#rbook_email").val() });
     });
 }
