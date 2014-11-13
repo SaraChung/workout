b1de113782fbc5c21633da3f83648efda4aa983f
Sara <rrrrrah@gmail.com>
2014-11-13 13:44:23 -0500

    removed refills-badges and cards

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018041520) do
+ActiveRecord::Schema.define(version: 20141009102922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
