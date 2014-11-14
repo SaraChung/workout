ca264af2c67c4818c0acf0e090cc70045d8721be
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-15 00:31:27 +0700

    add scope for sessions

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,11 @@
 class Session < ActiveRecord::Base
   belongs_to :user
   belongs_to :trainer
+
+  default_scope { eager_load(:users, :trainers) }
+  scope :whose, -> (trainer_id) { where trainer_id: trainer_id }
+  scope :accepted, -> { where status: true }
+  scope :declined, -> { where status: false }
+  scope :past_workouts, -> { where("to_when < ?", Time.now) }
+  scope :upcoming_workouts, -> { where("to_when > ?", Time.now) }
 end
