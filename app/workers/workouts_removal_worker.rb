45a7226bd5fe907e119a6a552da8a6f179efac6e
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-19 17:54:52 +0700

    add workout removal worker

---
+++ app/workers/workouts_removal_worker.rb
@@ -0,0 +1,11 @@
+class WorkoutsRemovalWorker
+  include Sidekiq::Worker
+  
+  def perform(subject_id, subject, obj_id, obj, session_id)
+    subj = subject == "Trainer" ? Trainer.find(subject_id) : User.find(subject_id)
+    target = obj == "Trainer" ? Trainer.find(subject_id) : User.find(subject_id)
+    session = Session.find(session_id)
+    push_notification = NotificationsService.new
+    push_notification.push_workout_removal(subj, subject, target, obj, session)
+  end
+end
