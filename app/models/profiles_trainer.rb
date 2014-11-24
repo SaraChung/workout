67d40ba8c23fbf03eb957e0d35a611baa59ce8ea
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-11-24 21:59:22 +0700

    add attachment

--- app/models/profiles_trainer.rb
+++ app/models/profiles_trainer.rb
@@ -1,2 +1,6 @@
 class ProfilesTrainer < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "175x175#", :thumb => "100x100>" }
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
